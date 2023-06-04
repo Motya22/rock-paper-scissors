1371809fc070b09127d4b41f80debd101b8f94bb
Motya22 <motya2222@gmail.com>
2023-06-04 15:26:03 +0300

    Add a playRound function
    Add a function that determines the winner and return it

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function getComputerChoice() {
 
   return choices[random];
 }
+
+function playRound(playerSelection, computerSelection) {
+  const playerSelectionInLowerCase = playerSelection.toLowerCase();
+
+  if (playerSelectionInLowerCase === computerSelection.toLowerCase()) {
+    return 'Draw!';
+  }
+
+  const rules = {
+    rock: 'Scissors',
+    paper: 'Rock',
+    scissors: 'Paper',
+  };
+  const playerSelectionInCapitalizeCase =
+    playerSelectionInLowerCase.charAt(0).toUpperCase() +
+    playerSelectionInLowerCase.slice(1);
+
+  if (computerSelection === rules[playerSelectionInLowerCase]) {
+    return `You Win! ${playerSelectionInCapitalizeCase} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelectionInCapitalizeCase}`;
+  }
+}
